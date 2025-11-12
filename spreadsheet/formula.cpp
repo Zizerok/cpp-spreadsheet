c2bdf5ba4f909190cc376532feebe766dea4f9d3
Zizerok <Zizerok2@mail.ru>
2025-11-12 17:36:14 +0300

    Implement Formula class for formula evaluation

--- spreadsheet/formula.cpp
+++ spreadsheet/formula.cpp
@@ -1,1 +1,59 @@
-/* разместите здесь свой код */
+#include "formula.h"
+
+#include "FormulaAST.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+#include <sstream>
+
+using namespace std::literals;
+
+namespace {
+class Formula : public FormulaInterface {
+public:
+    explicit Formula(std::string expression) : ast_(ParseFormulaAST(expression)) {
+        for (const Position& pos : ast_.GetCells()) {
+            referenced_cells_.insert(pos);
+        }
+    }
+
+    Value Evaluate(const SheetInterface& sheet) const override {
+        try {
+            return ast_.Execute(sheet);
+        } catch (const FormulaError& fe) {
+            return fe;
+        }
+    }
+
+    std::vector<Position> GetReferencedCells() const override {
+        std::vector<Position> ans;
+        for (const auto& pos : referenced_cells_) {
+            ans.push_back(pos);
+        }
+        std::sort(ans.begin(), ans.end());
+        return ans;
+    }
+
+    std::string GetExpression() const override {
+        std::stringstream out;
+        ast_.PrintFormula(out);
+        return out.str();
+    }
+    
+    
+    
+private:
+    std::unordered_set<Position, PositionHash> referenced_cells_;
+    FormulaAST ast_;
+};
+}  // namespace
+
+std::unique_ptr<FormulaInterface> ParseFormula(std::string expression) {
+    try {
+        return std::make_unique<Formula>(std::move(expression));
+    }
+    catch (...) {
+        throw FormulaException("");
+    }
+}
